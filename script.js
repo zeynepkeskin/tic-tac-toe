1ff37f1b862830e66fdbf401aeedfd2cd52e58c6
zeynepkeskin <zeynepkeskin10@gmail.com>
2023-04-02 22:12:42 -0700

    update2

--- script.js
+++ script.js
@@ -36,21 +36,21 @@ function clicked(elm) {
         alert("This space is already filled")
         return
     }
-    if (turn == "x") {
+    if (turn == "X") {
         elm.style.backgroundImage = "url('img/x.png')";
-        turn = "o"
+        turn = "O"
         elm.innerHTML = "O";
     }
     else {
         elm.style.backgroundImage = "url('img/o.png')";
         elm.innerHTML = "X";
-        turn = "x"
+        turn = "X"
 
     }
     for (i = 0; i < game.length; i++) {
         if (nums[i] == elm.className) {
-            if (turn == "x") game[i] = "o"
-            else game[i] = "x"
+            if (turn == "X") game[i] = "O"
+            else game[i] = "X"
         }
     }
     if (clicks > 1) {
@@ -61,28 +61,36 @@ function clicked(elm) {
 
 function checkGameOver() {
     for (i = 0; i < 7; i += 3) {
-        if (game[i] == "x" || game[i] == "o") {
+        if (game[i] == "X" || game[i] == "O") {
             if (game[i] == game[i + 1] && game[i + 1] == game[i + 2]) {
                 document.getElementById("winner").innerHTML = game[i] + " WINS"
                 document.getElementById("end").style.display = 'block';
+                document.getElementById("winner").style.display = 'none';
+                document.getElementById("playagain").style.display = 'none';
+
+                setTimeout(() => { 
+                    document.getElementById("smile").style.display = 'none'; 
+                    document.getElementById("winner").style.display = 'block';
+                    document.getElementById("playagain").style.display = 'block';
+                }, 2000);
             }
         }
     }
     for (i = 0; i < 4; i += 1) {
-        if (game[i] == "x" || game[i] == "o") {
+        if (game[i] == "X" || game[i] == "O") {
             if (game[i] == game[i + 3] && game[i + 3] == game[i + 6]) {
                 document.getElementById("winner").innerHTML = game[i] + " WINS"
                 document.getElementById("end").style.display = 'block';
             }
         }
     }
-    if (game[0] == "x" || game[0] == "o") {
+    if (game[0] == "X" || game[0] == "O") {
         if (game[0] == game[4] && game[4] == game[8]) {
             document.getElementById("winner").innerHTML = game[i] + " WINS"
             document.getElementById("end").style.display = 'block';
         }
     }
-    if (game[2] == "x" || game[2] == "o") {
+    if (game[2] == "X" || game[2] == "O") {
         if (game[2] == game[4] && game[4] == game[6]) {
             document.getElementById("winner").innerHTML = game[i] + " WINS"
             document.getElementById("end").style.display = 'block';
