5914710aa634017d90665402651fc4c40c75c0dd
zeynepkeskin <zeynepkeskin10@gmail.com>
2023-04-02 21:25:01 -0700

    update

--- script.js
+++ script.js
@@ -37,14 +37,15 @@ function clicked(elm) {
         return
     }
     if (turn == "x") {
-        elm.innerHTML = "X"
+        elm.style.backgroundImage = "url('img/x.png')";
         turn = "o"
-        elm.style.color = "red";
+        elm.innerHTML = "O";
     }
     else {
-        elm.innerHTML = "O"
-        turn = "x";
-        elm.style.color = "blue";
+        elm.style.backgroundImage = "url('img/o.png')";
+        elm.innerHTML = "X";
+        turn = "x"
+
     }
     for (i = 0; i < game.length; i++) {
         if (nums[i] == elm.className) {
@@ -61,19 +62,31 @@ function clicked(elm) {
 function checkGameOver() {
     for (i = 0; i < 7; i += 3) {
         if (game[i] == "x" || game[i] == "o") {
-            if (game[i] == game[i + 1] && game[i + 1] == game[i + 2]) alert(game[i] + " WINS")
+            if (game[i] == game[i + 1] && game[i + 1] == game[i + 2]) {
+                document.getElementById("winner").innerHTML = game[i] + " WINS"
+                document.getElementById("end").style.display = 'block';
+            }
         }
     }
     for (i = 0; i < 4; i += 1) {
         if (game[i] == "x" || game[i] == "o") {
-            if (game[i] == game[i + 3] && game[i + 3] == game[i + 6]) alert(game[i] + " WINS")
+            if (game[i] == game[i + 3] && game[i + 3] == game[i + 6]) {
+                document.getElementById("winner").innerHTML = game[i] + " WINS"
+                document.getElementById("end").style.display = 'block';
+            }
         }
     }
-    if(game[0] == "x" || game[0] == "o"){
-        if(game[0] == game[4] && game[4] == game[8]) alert(game[i] + " WINS")
+    if (game[0] == "x" || game[0] == "o") {
+        if (game[0] == game[4] && game[4] == game[8]) {
+            document.getElementById("winner").innerHTML = game[i] + " WINS"
+            document.getElementById("end").style.display = 'block';
+        }
     }
-    if(game[2] == "x" || game[2] == "o"){
-        if(game[2] == game[4] && game[4] == game[6]) alert(game[i] + " WINS")
+    if (game[2] == "x" || game[2] == "o") {
+        if (game[2] == game[4] && game[4] == game[6]) {
+            document.getElementById("winner").innerHTML = game[i] + " WINS"
+            document.getElementById("end").style.display = 'block';
+        }
     }
 }
 
